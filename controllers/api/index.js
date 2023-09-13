3fac3b915acc1f20cbbae8798f5820c33e28e6fe
MarkoAndreev <68433525+MarkoAndreev@users.noreply.github.com>
2023-09-12 19:47:08 -0500

    added code to userRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,11 @@
 const router = require("express").Router();
+
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes'); 
-const loginRoutes = require('./loginRoutes'); 
-const logoutRoutes = require('./logoutRoutes'); 
 const commentRoutes = require('./commentRoutes'); 
 
-
-
-
-router.use('/user', userRoutes); 
-router.use('/post', postRoutes); 
-router.use('/login', loginRoutes); 
-router.use('/logout', logoutRoutes); 
+router.use('/users', userRoutes); 
+router.use('/posts', postRoutes);
 router.use('/comment', commentRoutes)
-
-
 
 module.export=router
