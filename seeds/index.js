266414b0fd97e71b1bc51541d54d8b8a33f023c5
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-16 18:59:55 -0500

    inital commit: convering seeds from .js to .json

--- seeds/index.js
+++ seeds/index.js
@@ -1,27 +1,27 @@
 const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
+const { user, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
-const commentData = require('./commentData.json');
+// const commentData = require('../');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  const User = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  const posts = await Post.bulkCreate(postData, {
+  const Post = await Post.bulkCreate(postData, {
     individualHooks: true,
     returning: true,
   });
 
-  const comments = await Comment.bulkCreate(commentData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // const comments = await Comment.bulkCreate(commentData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
   process.exit(0);
 };
