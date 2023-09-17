2d641fef0c6e1f42aff1e2d7cc14b8863e07a504
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-09-17 16:09:27 -0500

    this commit converts seeds from js to .json with reruired changes to index (model and seed) js

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { user, Post, Comment } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
@@ -8,12 +8,12 @@ const postData = require('./postData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const User = await User.bulkCreate(userData, {
+  const user = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  const Post = await Post.bulkCreate(postData, {
+  const post = await Post.bulkCreate(postData, {
     individualHooks: true,
     returning: true,
   });
